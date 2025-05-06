3465c3cd8db8959b0b22898b0f54a12764d610ac
Herschel_TC <raphael.lesterlin@gmail.com>
2025-05-06 22:24:41 +0200

    uniformisation + completed faq page + updated header file + changed background design of mainpage to be more in line with other page, added things to be done to it too

---
+++ faq.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial_scale=1.0"/>
+        <link href="faq.css" rel="stylesheet"/>
+        <title>Politic'FAQ</title>
+    </head>
+    <body>
+
+        <?php include("header.html"); ?>
+
+        <section id="maincontent">
+
+            <h1>Frequently asked questions</h1>
+
+            <h2>What's this website for ?</h2>
+            <p>Politic'eye is a project aiming to bring to everyone clear information on our political representatives. Our content, aimed mostly at abstentionists or low-informed voters, is there to help people make their own judgment on the plethora of politicians in the public debate before voting. As goes the democratic principles, the consent of the people to be governed must be not only free, but informed.</p>
+
+            <h2>How do I use it ?</h2>
+            <p>Simply click on the "list" page to access the list of politicians currently registered in our database. As you navigate through the links, you will be presented with a report with elementary information on the person, their list of mandates, their conflict of interests, their scandals and condemnations. Politic'eye reminds that, as the presumption of innocence still holds, any report outside of the scope of the condemnations sections does not imply automatic guilt. It is only the information and investigations brought by our selected panel of media to provide viable information.</p>
+
+            <h2>Can I participate ?</h2>
+            <p>At the politic'eye team, we are all human beings. With our knowledge and interest différences, we cannot singlehandedly curate and report our findings on the database. Thus why, any visitor's contribution is welcome. Feel free to go to the Feedback page and give us any information, or message that you want to transmit to better the website. Outside contributions are fundamental to the experience we want to realize.</p>
+
+            <h2>Can I contact the team ?</h2>
+            <p>For the moment, the only option is to fill the feedback form with at least your contact and check that you want to be contacted. As time goes, we may put online an official contact for diverse inquiries, but due to safety reasons we prefer to have proper time to think about it.</p>
+
+            <h2>Can I join the project ?</h2>
+            <p>This would be absolutely amazing to bring new people to our cause. For the reasons mentioned above, it is quite uneasy to formally bring people into the team. Feel free to send a feedback with your contact information, and we shall process with internal vetting of your application.</p>
+
+            <h2>How are you financed ?</h2>
+            <p>We are currently receiving funds and support only through the sheer passion and benevolent support of our team members. No institutions nor corporate entity sponsors this website. If you would like to support us and get a thank you in this page, you may try to contact us. We will try to create a mechanism for our future supporters to help us as the projects grows.</p>
+
+        </section>
+
+        <section>
+            <span id="thx">Thank you for your support !</span>
+        </section>
+
+        <?php include("footer.html"); ?>
+
+    </body>
+</html>
--- mainpage.php
+++ mainpage.php
@@ -13,24 +13,28 @@
         <?php include("header.html"); ?>
 
         <section id="main_content">
-        <div class="container">
-            <h1 id="welcome">Bienvenue sur Politic'Eye</h1>
-            <p class="slogan">Analysez et explorez la politique en un clic</p>
-            <div class="search-box">
-                <input type="text" placeholder="Rechercher un sujet politique...">
-                <button>Rechercher</button>
+            <div class="container">
+                <h1 id="welcome">Bienvenue sur Politic'Eye</h1>
+                <p class="slogan">Analysez et explorez la politique en un clic</p>
+                <div class="search-box">
+                    <input type="text" placeholder="Rechercher un sujet politique...">
+                    <button>Rechercher</button>
+                </div>
             </div>
-        </div>
 
-        <section class="white-section">
-            <h2>À propos de Politic'Eye</h2>
-            <p class="p1">Politic'Eye est une plateforme dédiée à l'analyse et à la compréhension des enjeux politiques contemporains. Grâce à notre technologie avancée, nous vous offrons des analyses approfondies et des perspectives uniques sur les sujets qui façonnent notre monde.</p>
-            <p class="p2">Notre mission est de rendre l'information politique accessible et compréhensible pour tous. Que vous soyez un étudiant, un professionnel ou simplement curieux de la politique, Politic'Eye est là pour vous aider à naviguer dans le paysage complexe de l'actualité politique.</p>
-            <p class="p3">Nous croyons en l'importance de l'éducation politique et de la transparence. C'est pourquoi nous nous engageons à fournir des analyses impartiales et basées sur des données fiables.</p>
-            <p class="p4">Rejoignez-nous dans notre mission d'éclairer le débat public et de promouvoir une citoyenneté informée.</p>
-        </section>
+            <!-- Rewrite in english + replace search by list access button -->
 
-        <a href="fiche.php?Person=Hazebrouck">Fiche de Laurent Hazebrouck</a>
+            <div class="white-section">
+                <h2>À propos de Politic'Eye</h2>
+                <p class="p1">Politic'Eye est une plateforme dédiée à l'analyse et à la compréhension des enjeux politiques contemporains. Grâce à notre technologie avancée, nous vous offrons des analyses approfondies et des perspectives uniques sur les sujets qui façonnent notre monde.</p>
+                <p class="p2">Notre mission est de rendre l'information politique accessible et compréhensible pour tous. Que vous soyez un étudiant, un professionnel ou simplement curieux de la politique, Politic'Eye est là pour vous aider à naviguer dans le paysage complexe de l'actualité politique.</p>
+                <p class="p3">Nous croyons en l'importance de l'éducation politique et de la transparence. C'est pourquoi nous nous engageons à fournir des analyses impartiales et basées sur des données fiables.</p>
+                <p class="p4">Rejoignez-nous dans notre mission d'éclairer le débat public et de promouvoir une citoyenneté informée.</p>
+            </div>
+
+            <!-- Add featured section where there are some reports pinned here following recent news, manual admin choice -->
+
+            <a href="fiche.php?Person=Hazebrouck">Fiche de Laurent Hazebrouck</a>
         </section>
 
         <?php include("footer.html"); ?>
